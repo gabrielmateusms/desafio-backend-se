d91181afe6023a3945a82648a57e7f887c6e34f2
Gabriel Mateus <gabrielmsouza@outlook.com.br>
2024-03-23 16:51:56 -0300

    feat: initial commit

---
+++ src/main/java/com/github/gabrielmateusms/desafiobackendse/DesafioBackendSeApplication.java
@@ -0,0 +1,13 @@
+package com.github.gabrielmateusms.desafiobackendse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioBackendSeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioBackendSeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/gabrielmateusms/desafiobackendse/DesafioBackendSeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.gabrielmateusms.desafiobackendse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioBackendSeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
